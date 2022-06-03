11442c655e81d6b70720a05f122e634772670376
Richies Luong <richiesluong@gmail.com>
2022-06-03 02:03:07 -0400

    Added react frontend

--- src/main/java/com/richluong/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/richluong/awsimageupload/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.richluong.awsimageupload.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -19,4 +21,11 @@ public class UserProfileService {
         return userProfileDataAccessService.getUserProfiles();
     }
 
+    void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        // 1. Check if image is not empty
+        // 2. If file is an image
+        // 3. Check if user exists in our database
+        // 4. Grab some metadata from file if any
+        // 5. Store the image in s3 and update database (userProfileImageLink) with s3 image link
+    }
 }
